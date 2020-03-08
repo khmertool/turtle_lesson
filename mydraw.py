039f1dd3eb48c055b1816f5e8b878b1dda9fb23b
khmertool <46778258+khmertool@users.noreply.github.com>
2020-03-08 12:13:52 +0700

    update

---
+++ mydraw.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 29 14:37:32 2020
+
+@author: danhhong
+"""
+
+import turtle
+
+def DrawStar():
+    turtle.pensize(5)
+    turtle.color('red', 'yellow')
+    turtle.begin_fill()
+    
+    while True:
+        turtle.forward(300)
+        turtle.right(144)
+        if abs(turtle.pos()) < 1:
+            break
+    
+    turtle.end_fill()
+    
+    turtle.hideturtle()
+    turtle.done()
+ 
+def DrawSun():
+    turtle.pensize(2)
+    turtle.color('red', 'yellow')
+    turtle.begin_fill()
+    
+    while True:
+        turtle.forward(300)
+        turtle.right(170)
+        if abs(turtle.pos()) < 1:
+            break
+    
+    turtle.end_fill()
+    
+    turtle.hideturtle()
+    turtle.done()
+
+def DrawSquare():
+    turtle.pensize(5)
+    turtle.color('red', 'yellow')
+    turtle.begin_fill()
+    
+    for i in range(4):
+        turtle.forward(200)
+        turtle.right(90)
+        
+    turtle.end_fill()
+    turtle.hideturtle()
+    turtle.done()
+
---
+++ testing.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  7 15:51:43 2020
+
+@author: danhhong
+"""
+
+from mydraw import DrawStar, DrawSquare, DrawSun
+
+DrawSun()
