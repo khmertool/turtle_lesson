af6eae712ff25afbef0442336163f41c3b370974
khmertool <46778258+khmertool@users.noreply.github.com>
2020-03-14 16:10:56 +0700

    update

--- testing.py
+++ testing.py
@@ -1,15 +1,15 @@
 # practice to use function in mydraw.py file
 
-from mydraw import DrawStar, DrawSun, DrawSquare
+from mydraw import DrawStar, DrawSun, Pandas
 
 # type command to tell machine the the shaps
-command = input("Please, write:\nstar, sun or square? ")
+command = input("Please, write:\nstar, sun or pandas? ")
 
 if command == "star":
     DrawStar()
 elif command == "sun":
     DrawSun()
-elif command == "square":
-    DrawSquare()
+elif command == "pandas":
+    Pandas()
 else:
     print("Do nothing")
